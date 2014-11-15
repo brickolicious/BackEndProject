fb83c1cc692ebf4b5f762a296ac01a86721db441
Bart <bart.vandecandelaere@student.howerst.be>
2014-11-15 21:52:17 +0100

    Marker
    Added: a marker that you can drag and drop this gives you the address of
    the location and the coordinates of the marker position.
    Added: custom marker icon based on an SVG path

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,3 +1,32 @@
+var geocoder = new google.maps.Geocoder();
+
+function geocodePosition(pos) {
+    geocoder.geocode({
+        latLng: pos
+    }, function(responses) {
+        if (responses && responses.length > 0) {
+            updateMarkerAddress(responses[0].formatted_address);
+        } else {
+            updateMarkerAddress('Cannot determine address at this location.');
+        }
+    });
+}
+
+function updateMarkerStatus(str) {
+    document.getElementById('markerStatus').innerHTML = str;
+}
+
+function updateMarkerPosition(latLng) {
+    document.getElementById('info').innerHTML = [
+        latLng.lat(),
+        latLng.lng()
+    ].join(', ');
+}
+
+function updateMarkerAddress(str) {
+    document.getElementById('address').innerHTML = str;
+}
+
 function initialize() {
     console.log("In initialize maps function");
 
@@ -8,10 +37,7 @@ function initialize() {
         zoom: 14
     };
 
-
-
-
-
+    //options for geolocation
     var options = {
         enableHighAccuracy: true,
         timeout: 5000,
@@ -27,17 +53,69 @@ function initialize() {
         console.log("geolocation succes: lat->"+mapOptions.center.lat+" long->"+mapOptions.center.lng);
         var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 
+        //set marker and update info where marker is set
+        var latLng = new google.maps.LatLng(crd.latitude, crd.longitude);
+        dragAndDrop(map,latLng);
+
     };
 
     function error(err) {
         console.log('Geolocation failed');
         var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+
+        var latLng = new google.maps.LatLng(mapOptions.center.lat, mapOptions.center.lng);
+        dragAndDrop(map,latLng);
     };
 
-    navigator.geolocation.getCurrentPosition(success, error, options)
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
+
+    //deals with the drag and dropping of the marker
+    //also calls function to update frontend info
+    function dragAndDrop(map,latLng){
 
 
 
+        var marker = new google.maps.Marker({
+            position: latLng,
+            title: 'My location',
+            map: map,
+            draggable: true,
+            icon: pinSymbol("#5bc0de")
+        });
+
+        // Update current position info.
+        updateMarkerPosition(latLng);
+        geocodePosition(latLng);
+
+        // Add dragging event listeners.
+        google.maps.event.addListener(marker, 'dragstart', function() {
+            updateMarkerAddress('Dragging...');
+        });
+
+        google.maps.event.addListener(marker, 'drag', function() {
+            //updateMarkerStatus('Dragging...');
+            updateMarkerPosition(marker.getPosition());
+        });
+
+        google.maps.event.addListener(marker, 'dragend', function() {
+            //updateMarkerStatus('Drag ended');
+            geocodePosition(marker.getPosition());
+        });
+
+
+    }
+
+    function pinSymbol(color) {
+        return {
+            path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z M -2,-30 a 2,2 0 1,1 4,0 2,2 0 1,1 -4,0',
+            fillColor: color,
+            fillOpacity:1,
+            /*strokeColor:'#000',
+            strokeWeight: 2,*/
+            scale:1
+            };
+    }
 
 }
 
