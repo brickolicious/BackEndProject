b9f4b5de6422a414c47d308ee17d870073ca9c7d
Bart <bart.vandecandelaere@student.howerst.be>
2014-11-16 18:59:36 +0100

    Search with autocomplete
    Added: Search
    Added: Autocomplete for search
    Added: Position marker on search location and get local address

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -30,7 +30,8 @@ function updateMarkerAddress(str) {
 function initialize() {
     console.log("In initialize maps function");
 
-
+    var map;
+    var mymarker;
 
     var mapOptions = {
         center: { lat: -34.397, lng: 150.644},
@@ -51,20 +52,21 @@ function initialize() {
         mapOptions.center.lat = crd.latitude;
         mapOptions.center.lng = crd.longitude;
         console.log("geolocation succes: lat->"+mapOptions.center.lat+" long->"+mapOptions.center.lng);
-        var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+        map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 
         //set marker and update info where marker is set
         var latLng = new google.maps.LatLng(crd.latitude, crd.longitude);
         dragAndDrop(map,latLng);
-
+        searchAutoComplete();
     };
 
     function error(err) {
         console.log('Geolocation failed');
-        var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+        map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 
         var latLng = new google.maps.LatLng(mapOptions.center.lat, mapOptions.center.lng);
         dragAndDrop(map,latLng);
+        searchAutoComplete();
     };
 
     navigator.geolocation.getCurrentPosition(success, error, options);
@@ -76,7 +78,7 @@ function initialize() {
 
 
 
-        var marker = new google.maps.Marker({
+        mymarker = new google.maps.Marker({
             position: latLng,
             title: 'My location',
             map: map,
@@ -89,18 +91,18 @@ function initialize() {
         geocodePosition(latLng);
 
         // Add dragging event listeners.
-        google.maps.event.addListener(marker, 'dragstart', function() {
+        google.maps.event.addListener(mymarker, 'dragstart', function() {
             updateMarkerAddress('Dragging...');
         });
 
-        google.maps.event.addListener(marker, 'drag', function() {
+        google.maps.event.addListener(mymarker, 'drag', function() {
             //updateMarkerStatus('Dragging...');
-            updateMarkerPosition(marker.getPosition());
+            updateMarkerPosition(mymarker.getPosition());
         });
 
-        google.maps.event.addListener(marker, 'dragend', function() {
+        google.maps.event.addListener(mymarker, 'dragend', function() {
             //updateMarkerStatus('Drag ended');
-            geocodePosition(marker.getPosition());
+            geocodePosition(mymarker.getPosition());
         });
 
 
@@ -116,6 +118,91 @@ function initialize() {
             scale:1
             };
     }
+
+    function searchAutoComplete(){
+
+        var input = /** @type {HTMLInputElement} */(
+            document.getElementById('pac-input'));
+
+        var types = document.getElementById('type-selector');
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map);
+
+        var infowindow = new google.maps.InfoWindow();
+
+        var marker = mymarker;
+       /*var marker = new google.maps.Marker({
+            map: map,
+            anchorPoint: new google.maps.Point(0, -29)
+        });*/
+
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+            infowindow.close();
+            marker.setVisible(false);
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+                return;
+            }
+
+            // If the place has a geometry, then present it on a map.
+            if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+            } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(17);  // Why 17? Because it looks good.
+            }
+
+            /*marker.setIcon(({
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(35, 35)
+            }));*/
+            marker.setPosition(place.geometry.location);
+            marker.setVisible(true);
+
+            var address = '';
+            if (place.address_components) {
+                address = [
+                    (place.address_components[0] && place.address_components[0].short_name || ''),
+                    (place.address_components[1] && place.address_components[1].short_name || ''),
+                    (place.address_components[2] && place.address_components[2].short_name || '')
+                ].join(' ');
+            }
+
+            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+            infowindow.open(map, marker);
+
+
+
+            console.log("latlong:"+marker.position.lat+", "+marker.position.lng);
+            var latlngPos = new google.maps.LatLng(marker.position.lat(),marker.position.lng());
+            updateMarkerPosition(latlngPos);
+            geocodePosition(latlngPos);
+        });
+
+        // Sets a listener on a radio button to change the filter type on Places
+        // Autocomplete.
+        function setupClickListener(id, types) {
+            var radioButton = document.getElementById(id);
+            google.maps.event.addDomListener(radioButton, 'click', function() {
+                autocomplete.setTypes(types);
+            });
+        }
+
+        setupClickListener('changetype-all', []);
+        setupClickListener('changetype-address', ['address']);
+        setupClickListener('changetype-establishment', ['establishment']);
+        setupClickListener('changetype-geocode', ['geocode']);
+
+
+
+    }
+
 
 }
 
