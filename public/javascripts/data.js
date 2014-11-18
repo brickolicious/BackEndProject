2e55f338e266c17776d9882bc6f2b83972ff3c19
Bart <bart.vandecandelaere@student.howerst.be>
2014-11-19 00:57:03 +0100

    Cosmetic code changes
    Reminder to add domains

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,44 +1,49 @@
 
-var buttons = document.getElementById("sideMenu").getElementsByTagName("button");
+initData();
 
-for(var i=0;i<buttons.length-1;i++){
+function initData() {
 
-    buttons[i].addEventListener("click",function(e){
-        //console.log("value: "+this.value);
-        var alertObj = {
-            "alertType": this.value,
-            "latLng":currentLatLng
-        };
-        //console.log("bij klik stuur dit object: "+alertObj.alertType+", "+currentLatLng.lat);
-        /*$.ajax({
-            dataType: "json",
-            url: "http://localhost:8080/data/setPoint",
-            data: JSON.stringify(alertObj),
-            success: function (jsonObj) {
-                /*items = jsonObj.items;
-                console.log(items);*/
-          /*      console.log(jsonObj);
-            }
+    coupleListenersToAlertButtons();
+    getAllPointsForInitialPosition();
 
-        });*/
+};
 
 
-        $.ajax({
-            type: "get",
-            url: "http://localhost:8080/data/setPoint",
-            /*crossDomain:true,
-            dataType: "json",
-            contentType: "application/json; charset=UTF-8",*/
-            data:  JSON.stringify(alertObj)
-        }).done(function ( data ) {
-            //console.log("ajax callback response:" + data);
-            alert(data);
+
+function coupleListenersToAlertButtons(){
+    var buttons = document.getElementById("sideMenu").getElementsByTagName("button");
+
+    for (var i = 0; i < buttons.length - 1; i++) {
+
+        buttons[i].addEventListener("click", function (e) {
+            //console.log("value: "+this.value);
+            var alertObj = {
+                "alertType": this.value,
+                "latLng": currentLatLng
+            };
+
+
+            //set point - send point data to server
+            $.ajax({
+                type: "get",
+                url: "http://localhost:8080/data/setPoint",
+                /*crossDomain:true,
+                 dataType: "json",
+                 contentType: "application/json; charset=UTF-8",*/
+                data: JSON.stringify(alertObj)
+            }).done(function (data) {
+                //console.log("ajax callback response:" + data);
+                alert(data);
+
+            });
+
 
         });
 
+    }
+}
 
-
-
-    });
-
+function getAllPointsForLocation(){
+    //haal punten op voor currentLatLng indien deze verandered
+    //maar hoe aanroepen hier wnr map file eerst ingelezen wordt
 }
